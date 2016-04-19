82a32e7b9cc791713b5596c5827a7566048e5111
toNyx <tonyx-live@hotmail.fr>
2016-04-19 17:22:39 +0200

    + Chat.cs > Initial Implementation
    ~Maths.cs
    > Removed unused references

---
+++ CommunityLib/Chat.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Loki.Common;
+using Loki.Game;
+
+namespace CommunityLib
+{
+    public static class Chat
+    {
+        private static List<LokiPoe.InGameState.ChatPanel.ChatEntry> _oldMessages = new IndexedList<LokiPoe.InGameState.ChatPanel.ChatEntry>();
+
+        /// <summary>
+        /// Type a message in the chat ui.
+        /// </summary>
+        /// <param name="msg">the message to be typed</param>
+        /// <param name="closeChatUi">Close chat after sending (default : true)</param>
+        /// <returns>ChatResult</returns>
+        public static LokiPoe.InGameState.ChatResult SendChatMsg(string msg, bool closeChatUi = true)
+        {
+            if (!LokiPoe.InGameState.ChatPanel.IsOpened)
+                LokiPoe.InGameState.ChatPanel.ToggleChat();
+
+            if (!LokiPoe.InGameState.ChatPanel.IsOpened) return LokiPoe.InGameState.ChatResult.UiNotOpen;
+
+            var result = LokiPoe.InGameState.ChatPanel.Chat(msg);
+
+            if (closeChatUi)
+            {
+                if (LokiPoe.InGameState.ChatPanel.IsOpened)
+                    LokiPoe.InGameState.ChatPanel.ToggleChat();
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Return a list of message recieved since the last time it was caled.
+        /// </summary>
+        /// <returns> List of ChatEntry </returns>
+        public static List<LokiPoe.InGameState.ChatPanel.ChatEntry> GetNewChatMessages()
+        {
+            var tempMsg = LokiPoe.InGameState.ChatPanel.Messages;
+            var result = new IndexedList<LokiPoe.InGameState.ChatPanel.ChatEntry>();
+
+            result.AddRange(tempMsg.Where(msg => !_oldMessages.Contains(msg)));
+
+            _oldMessages = tempMsg;
+
+            return result;
+        }
+    }
+}
--- CommunityLib/Maths.cs
+++ CommunityLib/Maths.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using Loki.Bot;
 using Loki.Common;
 
 namespace CommunityLib
