35e01d58c5ad411a6d1d7a6a57e14f5b4c5589d1
toNyx <tonyx-live@hotmail.fr>
2016-04-19 19:18:27 +0200

    Attempt to fix shit from torm
    heuhue

--- CommunityLib/Navigation.cs
+++ CommunityLib/Navigation.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Buddy.Coroutines;
 using Loki.Bot.Logic.Bots.OldGrindBot;
