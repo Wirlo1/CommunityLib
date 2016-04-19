5128065ac5d4c1704702fa0d6c5f6bfe2441d38f
Tormiasz <tormiasz@gmail.com>
2016-04-19 13:19:56 +0200

    Data File

---
+++ CommunityLib/Data.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace CommunityLib
+{
+    public static class Data
+    {
+        public static readonly List<string> CurrencyList = new List<string>
+        {
+            "Scroll of Wisdom", "Portal Scroll", "Orb of Transmutation",
+            "Orb of Augmentation", "Orb of Alteration", "Jeweller's Orb",
+            "Armourer's Scrap", "Blacksmith's Whetstone", "Glassblower's Bauble",
+            "Cartographer's Chisel", "Gemcutter's Prism", "Chromatic Orb",
+            "Orb of Fusing", "Orb of Chance", "Orb of Alchemy", "Regal Orb",
+            "Exalted Orb", "Chaos Orb", "Blessed Orb", "Divine Orb",
+            "Orb of Scouring", "Orb of Regret", "Vaal Orb", "Mirror of Kalandra"
+        };
+    }
+}
