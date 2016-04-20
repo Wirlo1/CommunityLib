01c7596c1b2c3f1834d74119a2c4035f9fa763e7
D. Tony <z4z0u_fnk@hotmail.fr>
2016-04-20 12:48:21 +0200

    Tormiasz (#10)
    * Auto restart after installation

--- CommunityLib/CommunityLib.cs
+++ CommunityLib/CommunityLib.cs
@@ -1,9 +1,18 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using Buddy.Coroutines;
+using Exilebuddy;
+using IronPython.Modules;
 using Loki.Bot;
 using Loki.Common;
 using log4net;
+using Loki;
+using Loki.Game;
 
 namespace CommunityLib
 {
@@ -13,8 +22,41 @@ namespace CommunityLib
 
         #region Implementation of IRunnable
 
+        private void RestartBot()
+        {
+            //var args = Environment.GetCommandLineArgs();
+            var exe = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            var workingDir = Path.GetDirectoryName(exe);
+
+            var proc1 = new ProcessStartInfo
+            {
+                UseShellExecute = true,
+                FileName = exe,
+                Arguments = LokiPoe.Memory.Process.StartInfo.Arguments,
+                WindowStyle = ProcessWindowStyle.Normal
+            };
+            if (workingDir != null)
+                proc1.WorkingDirectory = workingDir;
+
+            Process.Start(proc1);
+            Application.Current.Shutdown((int)ApplicationExitCodes.Restarting);
+        }
+
         public void Start()
         {
+            if (_needRestart)
+            {
+                Log.InfoFormat("[{0}] ------------------------------------------------------------------------------", Name);
+                Log.InfoFormat("[{0}] ------------------------------------------------------------------------------", Name);
+                Log.InfoFormat("[{0}] ------------------------------------------------------------------------------", Name);
+                //Log.InfoFormat("[{0}] {1} has been successfuly installed. Please restart the bot completly.", Name, Name);
+                Log.InfoFormat("[{0}] {1} has been successfuly installed. The bot will now restart itself.", Name, Name);
+                Log.InfoFormat("[{0}] ------------------------------------------------------------------------------", Name);
+                Log.InfoFormat("[{0}] ------------------------------------------------------------------------------", Name);
+                Log.InfoFormat("[{0}] ------------------------------------------------------------------------------", Name);
+                //BotManager.Stop(true);
+                RestartBot();
+            }
             Log.DebugFormat("[{0}] Starting", Name);
         }
 
@@ -41,8 +83,29 @@ namespace CommunityLib
 
         #region Implementation of IBase
 
+        //Install's and enables itself
+        private bool _needRestart;
+        private void Install()
+        {
+            //Adding ourselves at the ContentLoader, wee need to be loaded before normal plugins
+            if (!GuiSettings.Instance.ContentOrder.Any(ent => ent.Name.Equals(Name)))
+            {
+                //Adding us on top
+                GuiSettings.Instance.ContentOrder.Add( new StringEntry{Name = Name });
+                _needRestart = true;
+            }
+
+            //Checking if we are in enabled
+            if (!GuiSettings.Instance.EnabledPlugins.Any(ent => ent.Equals(Name)))
+            {
+                GuiSettings.Instance.EnabledPlugins.Add(Name);
+                _needRestart = true;
+            }
+        }
+
         public void Initialize()
         {
+            Install();
             Log.DebugFormat("[{0}] Initialized", Name);
         }
 
