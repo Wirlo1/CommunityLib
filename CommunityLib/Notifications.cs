b5312fc35a9f2ee490e7e851fd1d36d003b8b810
Wirlo1 <pluu1@yandex.com>
2017-01-12 23:00:51 +0100

    Update Notifications.cs

--- CommunityLib/Notifications.cs
+++ CommunityLib/Notifications.cs
@@ -1,7 +1,8 @@
-﻿//!CompilerOption|AddRef|System.Web.dll
+//!CompilerOption|AddRef|System.Web.dll
 using System;
 using System.IO;
 using System.Net;
+using System.Threading;
 using System.Web;
 
 namespace CommunityLib
@@ -16,20 +17,26 @@ namespace CommunityLib
         /// <param name="ev">Event name</param>
         /// <param name="description">Event description</param>
         /// <param name="priority">Notification Priority</param>
-        /// <returns>NotificationResult enum entry</returns>
-        public static Results.NotificationResult Prowl(string apikey, string pluginName, string ev, string description, NotificationPriority priority)
+        /// <param name="onResult">delegate for error handling</param>
+        public static void Prowl(string apikey, string pluginName, string ev, string description, NotificationPriority priority, Action<Results.NotificationResult> onResult = null)
         {
-            if (!CheckApiToken(apikey))
-                return Results.NotificationResult.ApiKeyError;
+            new Thread(() =>
+            {
+                if (!CheckApiToken(apikey))
+                {
+                    onResult?.Invoke(Results.NotificationResult.ApiKeyError);
+                    return;
+                }
 
-            string url = "https://prowl.weks.net/publicapi/add";
-            url += "?apikey=" + HttpUtility.UrlEncode(apikey.Trim()) +
-                   "&application=" + HttpUtility.UrlEncode(pluginName) +
-                   "&description=" + HttpUtility.UrlEncode(description) +
-                   "&event=" + HttpUtility.UrlEncode(ev) +
-                   "&priority=" + HttpUtility.UrlEncode(priority.ToString());
+                string url = "https://prowl.weks.net/publicapi/add";
+                url += "?apikey=" + HttpUtility.UrlEncode(apikey.Trim()) +
+                       "&application=" + HttpUtility.UrlEncode(pluginName) +
+                       "&description=" + HttpUtility.UrlEncode(description) +
+                       "&event=" + HttpUtility.UrlEncode(ev) +
+                       "&priority=" + HttpUtility.UrlEncode(priority.ToString());
 
-            return Send(url);
+                onResult?.Invoke(Send(url));
+            }).Start();
         }
 
         /// <summary>
@@ -40,23 +47,32 @@ namespace CommunityLib
         /// <param name="description">Message to send</param>
         /// <param name="ev">Title</param>
         /// <param name="p">Notification Priority</param>
-        /// <returns></returns>
-        public static Results.NotificationResult Pushover(string token, string apikey, string description, string ev, NotificationPriority p)
+        /// <param name="onResult">delegate for error handling</param>
+        public static void Pushover(string token, string apikey, string description, string ev, NotificationPriority p, Action<Results.NotificationResult> onResult = null)
         {
-            if (!CheckApiToken(token))
-                return Results.NotificationResult.TokenError;
+            new Thread(() =>
+            {
+                if (!CheckApiToken(token))
+                {
+                    onResult?.Invoke(Results.NotificationResult.TokenError);
+                    return;
+                }
 
-            if (!CheckApiToken(apikey))
-                return Results.NotificationResult.ApiKeyError;
+                if (!CheckApiToken(apikey))
+                {
+                    onResult?.Invoke(Results.NotificationResult.ApiKeyError);
+                    return;
+                }
 
-            string url = "https://api.pushover.net/1/messages.json";
-            url += "?token=" + HttpUtility.UrlEncode(token.Trim()) +
-                   "&user=" + HttpUtility.UrlEncode(apikey.Trim()) +
-                   "&message=" + HttpUtility.UrlEncode(description) +
-                   "&title=" + HttpUtility.UrlEncode(ev) +
-                   "&priority=" + HttpUtility.UrlEncode(p.ToString());
-            
-            return Send(url);
+                string url = "https://api.pushover.net/1/messages.json";
+                url += "?token=" + HttpUtility.UrlEncode(token.Trim()) +
+                        "&user=" + HttpUtility.UrlEncode(apikey.Trim()) +
+                        "&message=" + HttpUtility.UrlEncode(description) +
+                        "&title=" + HttpUtility.UrlEncode(ev) +
+                        "&priority=" + HttpUtility.UrlEncode(p.ToString());
+
+                onResult?.Invoke(Send(url));
+            }).Start();
         }
 
         /// <summary>
@@ -65,19 +81,25 @@ namespace CommunityLib
         /// <param name="apikey"></param>
         /// <param name="description">Body of the notification</param>
         /// <param name="ev">Title of the notification</param>
-        /// <returns></returns>
-        public static Results.NotificationResult Pushbullet(string apikey, string description, string ev)
+        /// <param name="onResult">delegate for error handling</param>
+        public static void Pushbullet(string apikey, string description, string ev, Action<Results.NotificationResult> onResult = null)
         {
-            if (!CheckApiToken(apikey))
-                return Results.NotificationResult.ApiKeyError;
+            new Thread(() =>
+            {
+                if (!CheckApiToken(apikey))
+                {
+                    onResult?.Invoke(Results.NotificationResult.ApiKeyError);
+                    return;
+                }
 
-            const string url = "https://api.pushbullet.com/api/pushes";
-            var myCreds = new CredentialCache { { new Uri(url), "Basic", new NetworkCredential(apikey, "") } };
-            string postData = "type=note" +
-                              "&body=" + HttpUtility.UrlEncode(description) +
-                              "&title=" + HttpUtility.UrlEncode(ev);
+                const string url = "https://api.pushbullet.com/api/pushes";
+                var myCreds = new CredentialCache { { new Uri(url), "Basic", new NetworkCredential(apikey, "") } };
+                string postData = "type=note" +
+                                    "&body=" + HttpUtility.UrlEncode(description) +
+                                    "&title=" + HttpUtility.UrlEncode(ev);
 
-            return Send(url, postData, myCreds);
+                onResult?.Invoke(Send(url, postData, myCreds));
+            }).Start();
         }
 
         //returns the result to parse it in the main function
@@ -104,7 +126,7 @@ namespace CommunityLib
             {
                 postResponse = request.GetResponse();
             }
-            catch(WebException ex)
+            catch (WebException ex)
             {
                 var test = (HttpWebResponse)ex.Response;
                 if (test.StatusCode != HttpStatusCode.OK)
